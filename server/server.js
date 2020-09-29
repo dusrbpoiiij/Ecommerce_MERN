6898ecc1198f4064c09f78328f7b5a0b2710d296
yeonkyu <dusrbpoiiij@naver.com>
2020-09-29 20:17:18 +0900

    add user register

--- server/server.js
+++ server/server.js
@@ -1,29 +1,38 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 require('dotenv').config({
-    path: './config/index.env'
+	path: './config/index.env'
 })
 
-app.use(express.urlencoded({extended:true}));
+// MongoDB 
+const connectDB = require('./config/db');
+connectDB();
+
+app.use(bodyParser.json());
 app.use(morgan('dev'))
 app.use(cors())
 
 
+// routes 
+app.use('/api/user', require('./routes/auth.route'));
+
+
 app.get('/', (req, res) => {
-    res.send('test route => home page')
+	res.send('test route => home page')
 });
 
 // Page Not Founded
 app.use((req, res) => {
-    res.status(400).json({
-        msg: 'Page not founded'
-    })
+	res.status(400).json({
+		msg: 'Page not founded'
+	})
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+	console.log(`App listening on port ${PORT}`);
 });
