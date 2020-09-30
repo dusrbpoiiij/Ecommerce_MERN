a92ec7852458ff100123ff972046dec0890330b9
yeonkyu <dusrbpoiiij@naver.com>
2020-09-30 17:09:41 +0900

    Create and Search all Category

--- server/server.js
+++ server/server.js
@@ -18,7 +18,9 @@ app.use(cors())
 
 
 // routes 
-app.use('/api/user', require('./routes/auth.route'));
+app.use('/api/user/', require('./routes/auth.route'));
+app.use('/api/category/', require('./routes/category.route'));
+
 
 
 app.get('/', (req, res) => {
